ff44a9d7ee52063e2a945ac555972583a40f2ac0
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-10-18 11:25:53 -0400

    Using a Fake Auth Service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../authFake.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -22,4 +23,10 @@ export class HomeComponent implements OnInit {
     );
   }
 
+  onLogIn(){
+    this.authService.login();
+  }
+  onLogOut(){
+    this.authService.logout();
+  }
 }
